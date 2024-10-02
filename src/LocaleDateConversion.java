de94a506149de8f803173008d860fe68868a7794
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-10-02 13:49:17 -0400

    locale

--- src/LocaleDateConversion.java
+++ src/LocaleDateConversion.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class LocaleDateConversion {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class LocaleDateConversion {
+
+    public static void main(String[] args) {
+        Locale usLocale = Locale.US; // Define Locale for US
+        Locale franceLocale = Locale.FRANCE; // Define Locale for France
+        Locale japanLocale = Locale.JAPAN; // Define Locale for Japan
+
+        LocalDate now = LocalDate.now();
+
+        printFormattedDate(usLocale, now);
+        printFormattedDate(franceLocale, now);
+        printFormattedDate(japanLocale, now);
+    }
+
+
+    private static void printFormattedDate(Locale locale, LocalDate date) {
+        // TODO: Print the language and country of the locale using locale.getLanguage() and locale.getCountry()
+        System.out.println("Language: " + locale.getLanguage());
+        System.out.println("Country: " + locale.getCountry());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d yyyy", locale);
+        System.out.println(formatter.format(date));
+
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("EEEE MMMM d yyyy", locale);
+        System.out.println(formatter.format(date));
+
+        System.out.println();
+    }
 }
